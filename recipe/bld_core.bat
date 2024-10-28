60f512288842826182f6149033967b69c73f0de9
Laure Billon <laure.marie.billon@gmail.com>
2024-10-28 16:36:06 +0100

    cancel build of NAV and APP

--- recipe/bld_core.bat
+++ recipe/bld_core.bat
@@ -1,5 +1,5 @@
 
-"%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib"
+"%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib"  --app False --nav False
 if errorlevel 1 exit 1
 
 "%PYTHON%" setup.py install --prefix="%PREFIX%" --app False --nav False
--- recipe/build_core.sh
+++ recipe/build_core.sh
@@ -2,6 +2,6 @@
 
 export HDF5_DIR=${PREFIX}
 
-python setup.py build
+python setup.py build  --app False --nav False
 
 python setup.py install --prefix=${PREFIX} --app False --nav False
