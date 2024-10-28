307d7d2819ee2224a71b9bc4ddab3e72d555fb59
Laure Billon <laure.marie.billon@gmail.com>
2024-10-28 17:10:34 +0100

    Windows seems to not find python

--- recipe/bld_core.bat
+++ recipe/bld_core.bat
@@ -1,6 +1,6 @@
 
-"%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib"  --app False --nav False
+"%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib"
 if errorlevel 1 exit 1
 
-"%PYTHON%" setup.py install --prefix="%PREFIX%" --app False --nav False
+"%PYTHON%" setup.py install --prefix="%PREFIX%"
 if errorlevel 1 exit 1
