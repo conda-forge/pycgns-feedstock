183b14917d5b66ee991c2867623b304fcfeb267d
Mickael PHILIT <mickey.phy@gmail.com>
2024-11-01 22:18:30 +0100

    update core recipe

--- recipe/build_core.sh
+++ recipe/build_core.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+set -ex
 
 export HDF5_DIR=${PREFIX}
 
-python setup.py build  --app False --nav False
+mkdir builddir
 
-python setup.py install --prefix=${PREFIX} --app False --nav False
+python -m build -w -n -x \
+    -Cbuilddir=builddir \-Csetup-args=-Dnav=True -Csetup-args=-Dapp=True -Csetup-args=-Duse-vtk=True
+    -Cinstall-args=--tags=runtime,python-runtime,devel \
+    -Csetup-args=-Dapp=false \
+    -Csetup-args=-Dnav=false \
+    -Csetup-args=-Duse-vtk=false \
+    || (cat builddir/meson-logs/meson-log.txt && exit 1)
+
+python -m pip install dist/pycgns*.whl
