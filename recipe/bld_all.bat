a7ef1ea05ec04bfb79f7452bd46e4646c4335f51
Mickael PHILIT <mickey.phy@gmail.com>
2024-11-05 13:16:32 +0100

    Update release 6.3.1
    - update recipe for meson built pyCGNS
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/bld_all.bat
+++ recipe/bld_all.bat
@@ -1,7 +1,18 @@
+@echo on
 
-"%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib"
-if errorlevel 1 exit 1
+mkdir builddir
 
-"%PYTHON%" setup.py install --prefix="%PREFIX%"
-if errorlevel 1 exit 1
+:: -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+%PYTHON% -m build -w -n -x ^
+    -Cbuilddir=builddir ^
+    -Csetup-args=-Dapp=true ^
+    -Csetup-args=-Dnav=true ^
+    -Csetup-args=-Duse-vtk=true
+if %ERRORLEVEL% neq 0 exit 1
 
+:: `pip install dist\pycgns*.whl` does not work on windows,
+:: so use a loop; there's only one wheel in dist/ anyway
+for /f %%f in ('dir /b /S .\dist') do (
+    pip install %%f
+    if %ERRORLEVEL% neq 0 exit 1
+)
